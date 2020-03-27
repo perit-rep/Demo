4ae1c97b671df5798e929b49905b2ba60315c724
perit-rep <sky50673963@163.com>
2020-03-28 00:20:45 +0800

    "first commit from code"

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 
 int main( int argc, const char ** argv )
 {
+	// here wo use git to test
 	std::cout << "Hello World." << std::endl;
     
-	return 0;
+	// here we just give a value;
+	int ret = 5;
+
+	return ret;
 }
